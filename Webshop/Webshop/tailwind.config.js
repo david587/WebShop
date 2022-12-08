6ad87c77da46c14f853f6ac0000b3db09e2b8b45
Baráth Dávid <davidbarath08@gmail.com>
2022-12-08 12:10:22 +0100

    készül

--- Webshop/Webshop/tailwind.config.js
+++ Webshop/Webshop/tailwind.config.js
@@ -4,25 +4,29 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'hero': "url('./img/background_image.jpg')"
-      },
-      colors: {
-        'deepForest': '#243c5a',
-        'deepForest': {
-          100: '#cffafe',
-          200: '#a5f3fc',
-          300: '#67e8f9',
-          400: '#22d3ee',
-          500: '#06b6d4',
-          600: '#0891b2',
-          700: '#0e7490',
-          800: '#243c5a',
-          900: '#122021',
-        },
+    colors: {
+      'deepForest': '#243c5a',
+      'neon': '#E33926',
+      'white': '#ffff',
+      'deepForest': {
+        100: '#cffafe',
+        200: '#a5f3fc',
+        300: '#67e8f9',
+        400: '#22d3ee',
+        500: '#06b6d4',
+        600: '#0891b2',
+        700: '#0e7490',
+        800: '#243c5a',
+        900: '#122021'
       }
     },
+    screens:{
+      "sm": "480px",
+      "md":"768px",
+      "lg":"976px",
+      "xl": "1440px"
+    },
+    extend: {},
   },
   plugins: [],
 }
