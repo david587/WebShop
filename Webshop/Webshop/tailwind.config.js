be6fb75e08dddf801bd9d186830c6fbc401d5a1b
Baráth Dávid <davidbarath08@gmail.com>
2022-12-08 10:23:27 +0100

    Navbar stilus kész, home componentbe elkezdve

--- Webshop/Webshop/tailwind.config.js
+++ Webshop/Webshop/tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('./img/background_image.jpg')"
+      },
+      colors: {
+        'deepForest': '#243c5a',
+        'deepForest': {
+          100: '#cffafe',
+          200: '#a5f3fc',
+          300: '#67e8f9',
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2',
+          700: '#0e7490',
+          800: '#243c5a',
+          900: '#122021',
+        },
+      }
+    },
   },
   plugins: [],
 }
