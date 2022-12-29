8ef529b15356eefcb764416f41ad9ac1057d6971
David <davidbarath08@gmail.com>
2022-12-29 14:41:47 +0100

    frontend frissitve

--- Webshop/Webshop/src/app/home/home.component.ts
+++ Webshop/Webshop/src/app/home/home.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  path: string = '../assets/img/homeheadset.png';
+  alt: string = 'headset';
+
+  path1: string = '../assets/img/Series-xController.png';
+  alt1: string = 'Series-xController';
+
+  path2: string = '../assets/img/Hyper-xHeadset.png';
+  alt2: string = 'Hyper-xHeadset';
+
+  path3: string = '../assets/img/LogitechG-Pro.png';
+  alt3: string = 'LogitechG-Pro';
+
+  path4: string = '../assets/img/LogitechG502.png';
+  alt4: string = 'LogitechG502';
+
+
 }
