7e37eee28d1071f9a32e0b95bd144f8d2162192a
David <davidbarath08@gmail.com>
2022-12-29 12:03:03 +0100

    insert sql átirva, compatibilities tábla javitva

--- Webshop/DB/insert.sql
+++ Webshop/DB/insert.sql
@@ -1,40 +1,25 @@
 insert into keyboards (name, brand, price, details, img) 
 values ("Redragon", "Gipr", 1554, "new", LOAD_FILE('./img'));
 
-
-insert into compatibilities(pc, ps, nintendo_switch, xbox, keyboard_id )
-values (true, true, false, false,
-(select keyboards_id from keyboards where keyboards.name = "Razer")
-);
-
---mouses--
 insert into mouses (name, brand, price, details, img) 
 values ("g20", "Logitech", 6430, "new", LOAD_FILE('./img'));
 
-insert into compatibilities(pc, ps, nintendo_switch, xbox, mouse_id )
-values (true, true, false, false,
-(select mouses_id from mouses where mouses.name = "g20")
-);
-
---mousepads--
 insert into mousepads (name, brand, price, details, img) 
 values ("g20 mousepad", "Logitech", 3400, "new", LOAD_FILE('./img'));
 
-insert into compatibilities(pc, ps, nintendo_switch, xbox, mouse_id )
-values (true, true, false, false,
-(select mousepads_id from mousepads where mousepads.name = "g20 mousepads")
-);
-
---headsets--
 insert into headsets (name, brand, price, details, img) 
 values ("stringer", "HyperX", 17430, "new", LOAD_FILE('./img'));
 
-insert into compatibilities(pc, ps, nintendo_switch, xbox, mouse_id )
-values (true, true, true, false,
+
+insert into compatibilities(pc, ps, nintendo_switch, xbox, keyboard_id,mouse_id,mousepad_id,headset_id )
+values (
+    true, true, false, false,
+(select keyboards_id from keyboards where keyboards.name = "Redragon"),
+(select mouses_id from mouses where mouses.name = "g20"),
+(select mousepads_id from mousepads where mousepads.name = "g20 mousepad"),
 (select headsets_id from headsets where headsets.name = "stringer")
 );
 
---insert into categories
 insert into categories (keyboard_id,mouse_id,headset_id,mousepad_id)
 values
 (
@@ -44,14 +29,14 @@ values
     (select headsets_id from headsets where headsets.name = "stringer")
 );
 
---users table
+
 INSERT INTO users (email, password, confirm_password, timestamps)
 values
 (
     "david@gmail.com","1234","1234","2022-08-04"
 );
 
---carts table
+
 INSERT INTO carts (quantity,categorie_id,user_id)
 values
 (
@@ -60,7 +45,7 @@ values
     (select users_id from users where users.email = "david@gmail.com")
 );
 
---buyer datas
+
 INSERT into buyers (name,email,address,phone,card,user_id)
 values
 (
@@ -68,7 +53,7 @@ values
     (select users_id from users where users.email = "david@gmail.com")
 );
 
---purcases
+
 INSERT INTO purcases (timestamps,productName,productBrand,productPrice,buyerName,buyerEmail,buyerAddress,cart_id,buyer_id)
 values
 (
