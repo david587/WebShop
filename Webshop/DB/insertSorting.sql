4c8c04ca5ca01ecdb8e33a29728142732aedb0ad
David <davidbarath08@gmail.com>
2023-01-19 13:28:40 +0100

    brands és categories tábla adatfeltöltési kódja kész

---
+++ Webshop/DB/insertSorting.sql
@@ -0,0 +1,13 @@
+insert into brands(brand)
+values
+("Logitech"),
+("Hp"),
+("Urage"),
+("Redragon");
+
+insert into categories(categorie)
+values
+("Keyboard"),
+("Mouse"),
+("Headset"),
+("Monitor");
