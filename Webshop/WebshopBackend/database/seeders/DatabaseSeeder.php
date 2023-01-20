3bac3483be7038fb98bffee708ffd333c080773a
David <davidbarath08@gmail.com>
2023-01-20 20:02:03 +0100

    Db folder deleted, because we dont need anymore->brandFactroy and categorie factory created and the seeder call these factories

--- Webshop/WebshopBackend/database/seeders/DatabaseSeeder.php
+++ Webshop/WebshopBackend/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
+        \App\Models\Brand::factory(4)->create();
+        \App\Models\Categorie::factory(4)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
