d7e899a5e20ec7cedc949ca3df1e0e5951f2b1b2
Baráth Dávid <61178364+david587@users.noreply.github.com>
2023-04-28 10:27:47 +0200

    Update webshop.sql

--- Webshop/DB/webshop.sql
+++ Webshop/DB/webshop.sql
@@ -1,121 +1,414 @@
--- Táblák felvételének sorrendje
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Gép: localhost
+-- Létrehozás ideje: 2023. Ápr 28. 10:27
+-- Kiszolgáló verziója: 10.4.27-MariaDB
+-- PHP verzió: 8.1.12
 
--- 1. compatibilities, 2. keyboards, 3. mouses, 4. mousepads,
---5. headsets, 6. categories, 7. carts, 8. users, 9. buyers,
---10. purcases
-
-drop database if exists webshop;
-create database webshop
-collate utf8_hungarian_ci
-charter set utf8;
-
-grant all privileges 
-on webshop.*
-to webshop@localhost
-identified by "Almafa12;";
-
-create table keyboards (
-    keyboards_id int not null primary key auto_increment,
-    name varchar(50),
-    brand varchar(50),
-    price int,
-    details varchar(500),
-    img longblob
-);
-
-create table mouses (
-    mouses_id int not null primary key auto_increment,
-    name varchar(50),
-    brand varchar(50),
-    price int,
-    details varchar(500),
-    img longblob
-);
-create table mousepads (
-    mousepads_id int not null primary key auto_increment,
-    name varchar(50),
-    brand varchar(50),
-    price int,
-    details varchar(500),
-    img longblob
-);
-create table headsets (
-    headsets_id int not null primary key auto_increment,
-    name varchar(50),
-    brand varchar(50),
-    price int,
-    details varchar(500),
-    img longblob
-);
-
-create table compatibilities (
-    compatibilities_id int not null primary key auto_increment,
-    pc boolean,
-    ps boolean,
-    PlayStation boolean,
-    xbox boolean,
-    keyboard_id integer,
-    mouse_id integer,
-    mousepad_id integer,
-    headset_id integer,
-    foreign key(keyboard_id) references keyboards (keyboards_id),
-    foreign key(mouse_id) references mouses (mouses_id),
-    foreign key(mousepad_id) references mousepads (mousepads_id),
-    foreign key(headset_id) references headsets (headsets_id)
-);
-create table categories (
-    categories_id int not null primary key auto_increment,
-    keyboard_id int,
-    mouse_id int,
-    mousepad_id int,
-    headset_id int,
-    foreign key (keyboard_id) references keyboards (keyboards_id),
-    foreign key (mouse_id) references mouses (mouses_id),
-    foreign key (mousepad_id) references mousepads (mousepads_id),
-    foreign key (headset_id) references headsets (headsets_id)
-);
-create table users (
-    users_id int not null primary key auto_increment,
-    name varchar(50),
-    email varchar(50),
-    password varchar(50),
-    confirm_password varchar(50),
-    timestamps date
-);
-create table carts (
-    carts_id int not null primary key auto_increment,
-    quantity int,
-    productName varchar(50),
-    productPrice integer,
-    productBrand varchar(50),
-    categorie_id integer,
-    user_id integer,
-    foreign key (categorie_id) references categories (categories_id),
-    foreign key (user_id) references users (users_id)
-);
-create table buyers (
-    buyers_id int not null primary key auto_increment,
-    name varchar(50),
-    email varchar(50),
-    address varchar(50),
-    phone varchar(50),
-    card varchar(50),
-    user_id integer,
-    foreign key (user_id) references users (users_id)
-);
-create table purcases (
-    purcases_id int not null primary key auto_increment,
-    timestamps date,
-    cart_id integer,
-    buyer_id integer,
-    productName varchar(50),
-    productPrice integer,
-    productBrand varchar(50),
-    buyerName varchar(50),
-    buyerEmail varchar(50),
-    buyerAddress varchar(50),
-    foreign key (cart_id) references carts (carts_id),
-    foreign key (buyer_id) references buyers (buyers_id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `webshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `brands`
+--
+
+CREATE TABLE `brands` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `brand` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- A tábla adatainak kiíratása `brands`
+--
+
+INSERT INTO `brands` (`id`, `brand`) VALUES
+(1, 'Logitech'),
+(2, 'Hp'),
+(3, 'Redragon'),
+(4, 'Urage');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `cart_items`
+--
+
+CREATE TABLE `cart_items` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `categorie` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- A tábla adatainak kiíratása `categories`
+--
+
+INSERT INTO `categories` (`id`, `categorie`) VALUES
+(1, 'Monitor'),
+(2, 'Keyboard'),
+(3, 'Headset'),
+(4, 'Mouse');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- A tábla adatainak kiíratása `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_01_18_120653_create_products_table', 1),
+(6, '2023_01_18_120739_create_brands_table', 1),
+(7, '2023_01_18_120824_create_categories_table', 1),
+(8, '2023_01_18_120906_create_order_informations_table', 1),
+(9, '2023_01_18_120907_create_orders_table', 1),
+(10, '2023_01_18_120959_create_cart_items_table', 1),
+(11, '2023_01_30_055244_create_new_letters_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `news_letters`
+--
+
+CREATE TABLE `news_letters` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `order_information_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `order_informations`
+--
+
+CREATE TABLE `order_informations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `shippingAddress` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `paymentMethod` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- A tábla adatainak kiíratása `personal_access_tokens`
+--
+
+INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
+(1, 'App\\Models\\User', 1, 'MyAuthApp', '3adabdc203b30b67b54b3bd6a111cf30e08228de9a6ad1a74100f59ff1133514', '[\"*\"]', '2023-04-28 06:24:37', NULL, '2023-04-28 06:20:19', '2023-04-28 06:24:37'),
+(2, 'App\\Models\\User', 1, 'MyAuthApp', '67268cb883e219a0c5d5aaf12276632e541740bac86c423eb68cbff62c8aebea', '[\"*\"]', NULL, NULL, '2023-04-28 06:26:26', '2023-04-28 06:26:26');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `details` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `inStock` int(11) NOT NULL,
+  `brand_id` bigint(20) UNSIGNED NOT NULL,
+  `categorie_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- A tábla adatainak kiíratása `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `details`, `image`, `inStock`, `brand_id`, `categorie_id`) VALUES
+(1, 'omen', 1534, 'new monitor', 'assets/images/hp_omen.png', 10, 2, 1),
+(2, 'g203', 1423, 'new mouse', 'assets/images/logitech_g203.png', 10, 1, 4),
+(3, 'k552Kumara', 1234, 'new keyboard', 'assets/images/redragon_k552kumara.png', 10, 3, 2),
+(4, 'sundz400', 1243, 'new headset', 'assets/images/urage_soundz400.png', 10, 4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `admin_since` timestamp NULL DEFAULT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `address`, `admin_since`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'david', 'david@gmail.com', 'AranyHegy', '2023-04-28 06:17:43', '2023-04-28 06:17:43', '$2y$10$qtrCEGI9/Gy2gZMrQ2bCrOeMXALJ/hw5tPOeAloAOKmRzPqBVbbiK', 'fyCG9NhXvx', '2023-04-28 06:17:43', '2023-04-28 06:17:43');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `cart_items`
+--
+ALTER TABLE `cart_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- A tábla indexei `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `news_letters`
+--
+ALTER TABLE `news_letters`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `order_informations`
+--
+ALTER TABLE `order_informations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- A tábla indexei `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- A tábla indexei `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_name_unique` (`name`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT a táblához `cart_items`
+--
+ALTER TABLE `cart_items`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT a táblához `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT a táblához `news_letters`
+--
+ALTER TABLE `news_letters`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `order_informations`
+--
+ALTER TABLE `order_informations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT a táblához `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
